1fc6708db8db8b63fdf2dd52a618a9302813c0bc
Siarhei Astapenka <Siarhei_Astapenka@epam.com>
2025-08-06 01:30:38 +0300

    Microservice Architecture Overview. Module 3. - Implement eureka

--- resource-service/src/main/java/com/epam/learn/resource_service/configuration/MetadataRestTemplateConfig.java
+++ resource-service/src/main/java/com/epam/learn/resource_service/configuration/MetadataRestTemplateConfig.java
@@ -1,22 +1,17 @@
 package com.epam.learn.resource_service.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MetadataRestTemplateConfig {
-    @Value("${song.metadata.service.url}")
-    private String serviceUrl;
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .rootUri(serviceUrl)
-                .defaultHeader("Content-Type", "application/json")
-                .build();
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
 
